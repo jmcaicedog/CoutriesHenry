3e0486dca1358f8858b6d84cebdd3d10051e25f6
Juan Manuel Caicedo <juanamanuelcaicedo@hotmail.com>
2023-01-20 09:30:01 -0500

    DB_PORT changed by PORT

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
-const DB_PORT = process.env.DB_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const getCountries = require("./src/helpers/getCountries");
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(DB_PORT, () => {
-    console.log(`%s listening at port ${DB_PORT}`);
+  server.listen(PORT, () => {
+    console.log(`%s listening at port ${PORT}`);
     getCountries();
   });
 });
